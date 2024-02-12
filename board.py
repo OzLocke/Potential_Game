2540b8144468802f0e245416e6964cd3a6b68576
ozlocke <oz.locke@gmail.com>
2024-02-12 18:04:51 +0000

    Amended to combine everything into a Table object, so the Table can control everything

--- board.py
+++ board.py
@@ -1,72 +1,67 @@
 # Import math library
 import math
 
-class Players():
-    def __init__(self, number):
-        self.number = number
-
-class Board():
-    #size = 7 defines a default value for the variable
-    def __init__(self, size = 7):
-        self.size = size
-
-    def generate_board(self):
-        #First column number of top row
-        self.start_col = math.floor(self.size/2)
-        #Number of columns in first row
-        self.cols = math.ceil(self.size/2)
-        #Row number of middle row
-        self.midway = math.ceil(self.size/2) - 1
-        #Empty array to hold board
-        self.board = []
-        #Create each row
-        for r in range(self.size):
-            #Create each column (q) in the row, appending each to the board
-            for q in range(self.start_col, self.start_col + self.cols):
-                self.board.append([q,r])
-            #Decrement starting column number (min 0)
-            if self.start_col > 0:
-                self.start_col -= 1
-            #Decrement or increment row number based on whether above or bellow midway
-            if r < self.midway:
-                self.cols += 1
-            else:
-                self.cols -= 1
-
-class Pieces():
-    def __init__(self, player, charge = 3):
-        self.player = player
-        self.charge = charge
-        
 class Table():
-    def __init__(self, players, board, pieces):
-        self.players = players
-        self.board = board
-        self.pieces = pieces
+    def __init__(self, player_count = 2, board_size = 7):
+        self.player_count = player_count
+        self.board_size = board_size
+        
+        #Create the board
+        self.board = self.Board(board_size)
 
-#--Generate Players--#
-player_count = 2
-players = {}
-#Loop over number of players, creating an instance for each
-#   and assigning them to dynamic variables via a dictionary
-for n in range(player_count):
-    players["player{0}".format(n)] = Players(n)
+        #Create the players assigning them to dynamic variables via a dictionary
+        self.players = {}
+        for n in range(player_count):
+            #Define player name (e.g. player1)
+            player = "player{0}".format(n)
+            #Add a variable to players and assign an instance of Players
+            self.players[player] = self.Players(player, self.board.size - 1)
+
+    class Players():
+        def __init__(self, player, piece_count):
+            self.player = player
+            self.piece_count = piece_count
+            #Create pieces assigned to the player
+            self.pieces = {}
+            for n in range(piece_count):
+                self.pieces["piece{0}".format(n)] = self.Pieces(self.player)
+
+        class Pieces():
+            def __init__(self, charge = 3):
+                self.charge = charge
+
+    class Board():
+        def __init__(self, size = 7):
+            self.size = size
+            #Create the board
+            #First column number of top row
+            self.start_col = math.floor(self.size/2)
+            #Number of columns in first row
+            self.cols = math.ceil(self.size/2)
+            #Row number of middle row
+            self.midway = math.ceil(self.size/2) - 1
+            #Empty array to hold board
+            self.board = []
+            #Create each row
+            for r in range(self.size):
+                #Create each column (q) in the row, appending each to the board
+                for q in range(self.start_col, self.start_col + self.cols):
+                    self.board.append([q,r])
+                #Decrement starting column number (min 0)
+                if self.start_col > 0:
+                    self.start_col -= 1
+                #Decrement or increment row number based on whether above or bellow midway
+                if r < self.midway:
+                    self.cols += 1
+                else:
+                    self.cols -= 1
 
 #--Generate a Board--#
-board = Board()
-board.generate_board()
+player_count = 2
+board_size = 7
+table = Table(player_count, board_size)
 
-#--Generate Pieces--#
-pieces = {}
-#Loop over players, creating instances of pieces for each
-#   equal to the board size and assigning them to dynamic variables via a dictionary
-for player in players.keys():
-    for n in range(board.size):
-        pieces["{0}_piece{1}".format(player, n)] = Pieces(player)
-
-#--Put Everything on the Table!--#
-table = Table(players, board, pieces)
-
+#And look it all over
 print(table.players.keys())
 print(table.board.board)
-print(table.pieces.keys())
+print(table.players["player1"].pieces.keys())
