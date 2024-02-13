c31bc279c328146b21cb4bf6b9e05b9be0311891
ozlocke <oz.locke@gmail.com>
2024-02-13 15:51:26 +0000

    Objects stored in arrays are now stored directly rather than against variables

--- board.py
+++ board.py
@@ -2,33 +2,38 @@
 import math
 
 class Table():
-    def __init__(self, player_count = 2, board_size = 7):
-        self.player_count = player_count
+    def __init__(self, board_size = 7):
         self.board_size = board_size
         
         #Create the board
         self.board = self.Board(board_size)
 
         #Create the players assigning them to dynamic variables via a dictionary
-        self.players = {}
-        for n in range(player_count):
-            #Define player name (e.g. player1)
-            player = "player{0}".format(n)
-            #Add a variable to players and assign an instance of Players
-            self.players[player] = self.Players(player, self.board.size - 1)
+        self.players = []
+        for n in range(2):
+            self.players.append(self.Players(n, self.board.size - 2))
+
+        #Put player pieces on the board
+        #Find width of top row
+        #Load pieces into each space
+        #Find next row
+        #Load pieces into every other row
+        #Repeat for bottom row for other player
 
     class Players():
         def __init__(self, player, piece_count):
-            self.player = player
+            self.name = "player{0}".format(player)
             self.piece_count = piece_count
-            #Create pieces assigned to the player
-            self.pieces = {}
+            #Create pieces assigned to the player, assigning them to dynamic variables via a dictionary
+            self.pieces = []
             for n in range(piece_count):
-                self.pieces["piece{0}".format(n)] = self.Pieces(self.player)
+                self.pieces.append(self.Pieces(n))
 
         class Pieces():
-            def __init__(self, charge = 3):
+            def __init__(self, name, charge = 3):
+                self.name = "piece{0}".format(name)
                 self.charge = charge
+                
 
     class Board():
         def __init__(self, size = 7):
@@ -41,12 +46,12 @@ class Table():
             #Row number of middle row
             self.midway = math.ceil(self.size/2) - 1
             #Empty array to hold board
-            self.board = []
+            self.spaces = []
             #Create each row
             for r in range(self.size):
                 #Create each column (q) in the row, appending each to the board
                 for q in range(self.start_col, self.start_col + self.cols):
-                    self.board.append([q,r])
+                    self.spaces.append(self.Spaces(q,r))
                 #Decrement starting column number (min 0)
                 if self.start_col > 0:
                     self.start_col -= 1
@@ -55,13 +60,23 @@ class Table():
                     self.cols += 1
                 else:
                     self.cols -= 1
+        
+        class Spaces():
+            def __init__(self, col, row):
+                self.col = col
+                self.row = row
+                self.piece = None
+                self.name = "{0},{1}".format(col,row)
 
 #--Generate a Board--#
-player_count = 2
-board_size = 7
-table = Table(player_count, board_size)
+table = Table()
 
 #And look it all over
-print(table.players.keys())
-print(table.board.board)
-print(table.players["player1"].pieces.keys())
+for player in table.players:
+    print(player.name)
+    for piece in player.pieces:
+        print(" " + piece.name)
+        print("  Charge: " + str(piece.charge))
+# for obj in table.board.spaces:
+#     print(obj.name)
+# print(table.players["player0"].pieces["piece0"].charge)
