23e3d4f229dce044779aa0cf8e0fc9d7583bf1f9
ozlocke <oz.locke@gmail.com>
2024-03-14 17:35:56 +0000

    Pieces can now be clicked on to show available moves, but there's a duplication issue

--- game/piece.gd
+++ game/piece.gd
@@ -3,12 +3,11 @@ extends Area2D
 var colours = [Color("Red",0.5),Color("Blue", 0.5)]
 var charge
 var location
-var hover = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	charge = 3
-	$Piece.modulate = colours[get_parent().player_number]
+	$Sprite.modulate = colours[get_parent().player_number]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -16,13 +15,7 @@ func _process(_delta):
 
 func _on_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_released("Click"):
-		charge -= 1
-		$Label.text = str(charge)
+		for space in get_tree().get_nodes_in_group("spaces"):
+			space.modulate = "ffffff6c"
+		get_parent().available_moves(self)
 
-
-func _on_mouse_entered():
-	hover = true
-
-
-func _on_mouse_exited():
-	hover = false
